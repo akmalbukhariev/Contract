a720f979c93a5755304fa2b2d133afe7b7664349
akmal <akmalbukhariev@gmail.com>
2022-05-23 22:00:52 +0900

    Add project files.

---
+++ Contract/Contract/Pages/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Contract.Pages
+{
+    public partial class MainPage : IPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Contract/Contract/Pages/PageLanguage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Contract.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contract.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLanguage : IPage
+    {
+        public PageLanguage()
+        {
+            InitializeComponent();
+        }
+
+        private void Item_Tapped(object sender, EventArgs e)
+        {
+            ClickAnimationView((ViewLanguage)sender);
+        }
+    }
+}
